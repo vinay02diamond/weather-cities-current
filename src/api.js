113f01aa09639ec66c77bc433acac2e8590b33ac
Vinay Krishna <vinaydiamod02@gmail.com>
2025-03-02 22:08:34 +0530

    Initial commit: Added weather app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bdebac67damsh8ccb3b00ae7c185p1044b5jsn2c7e1df4486e",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "63a3d34a65bb63b880b725e846d0aa70"; // enter your key from openweather API
